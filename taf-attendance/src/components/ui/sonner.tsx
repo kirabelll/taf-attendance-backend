e59b52a9ca54cce2f46bcd9901a6e01b514500b1
kirubel kassahun <kirubelkassahun9@gmail.com>
2026-05-11 12:09:35 +0300

    refactor(Toaster): update theme default to 'light' and adjust imports

--- taf-attendance/src/components/ui/sonner.tsx
+++ taf-attendance/src/components/ui/sonner.tsx
@@ -1,10 +1,11 @@
-import { useTheme } from "next-themes";
+import * as React from "react";
+import { useTheme } from "@/hooks/useTheme";
 import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
+  const { theme = "light" } = useTheme();
 
   return (
     <Sonner
